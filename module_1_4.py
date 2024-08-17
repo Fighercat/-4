e9b7c5988f7350f9803ff2a840b876471576238e
Fighercat <artem.sitnikov.2002@list.ru>
2024-08-17 17:44:56 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input()
+print(len(my_string),'= длина строки')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))#как же я задрался вводить __old: и __new: в ручную, тк читал писменную лекцию и не понимал где ошибка, а не видеоурок,а ларчик просто открывался
+print(my_string[0])
+print(my_string[-1])
